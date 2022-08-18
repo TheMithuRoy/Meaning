e9482a63ee5c5ad3d6a55187913c474ca614d6e1
Mithu Roy <mithu.roy@cognizant.com>
2022-08-19 04:50:04 +0530

    Handles error, progress and no data state

--- app/src/main/java/com/cognizant/meaning/presentation/ui/composable/MeaningSearchScreen.kt
+++ app/src/main/java/com/cognizant/meaning/presentation/ui/composable/MeaningSearchScreen.kt
@@ -1,15 +1,24 @@
 package com.cognizant.meaning.presentation.ui.composable
 
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.cognizant.meaning.R
 import com.cognizant.meaning.presentation.viewmodel.MeaningViewModel
 
 @Composable
@@ -23,22 +32,48 @@ fun MeaningSearchScreen(
         modifier = Modifier.fillMaxSize(),
         color = MaterialTheme.colors.background
     ) {
-        Column {
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
             SearchBar(
                 value = sfQuery,
                 onValueChange = viewModel::onQueryChange
             )
-            LazyColumn(
-                modifier = Modifier.fillMaxWidth(),
-                contentPadding = PaddingValues(8.dp),
-                verticalArrangement = Arrangement.spacedBy(8.dp)
-            ) {
-                items(uiState.meanings) { meaning ->
-                    MeaningItem(meaning = meaning)
+            if (uiState.isLoading) {
+                CircularProgressIndicator()
+            } else if (uiState.noDataAvailable) {
+                NoData(message = "No Data Found", imageId = R.drawable.ic_no_data)
+            } else if (uiState.error.isNotBlank()) {
+                NoData(message = uiState.error, imageId = R.drawable.ic_error)
+            } else {
+                LazyColumn(
+                    modifier = Modifier.fillMaxWidth(),
+                    contentPadding = PaddingValues(8.dp),
+                    verticalArrangement = Arrangement.spacedBy(8.dp)
+                ) {
+                    items(uiState.meanings) { meaning ->
+                        MeaningItem(meaning = meaning)
+                    }
                 }
             }
         }
     }
+}
+
+@Composable
+private fun NoData(
+    message: String,
+    @DrawableRes imageId: Int,
+    modifier: Modifier = Modifier
+) {
+    Spacer(modifier = Modifier.size(8.dp))
+    Image(
+        painter = painterResource(id = imageId),
+        contentDescription = null,
+        modifier = modifier
+            .scale(0.5f)
+            .wrapContentSize()
+    )
+    Spacer(modifier = Modifier.size(8.dp))
+    Text(text = message, fontWeight = FontWeight.SemiBold)
 }
 
 @Preview
